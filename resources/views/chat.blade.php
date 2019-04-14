a1d1bf936a0a948911e7154f3b64c4aa4070674f
NJTAKAYUKI <nj.takayuki@gmail.com>
2019-04-14 15:08:07 +0000

    メッセージ表示できた

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,6 +4,19 @@
         <textarea v-model="message"></textarea>
         <br>
         <button type="button" @click="send()">送信</button>
+
+        <hr>
+
+        <div v-for="m in messages">
+
+            <!-- 登録された日時 -->
+            <span v-text="m.created_at"></span>：&nbsp;
+
+            <!-- メッセージ内容 -->
+            <span v-text="m.body"></span>
+
+        </div>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
@@ -12,22 +25,39 @@
         new Vue({
             el: '#chat',
             data: {
-                message: ''
+                message: '',
+                messages: []
             },
             methods: {
+                getMessages() {
+
+                    const url = '/ajax/chat';
+                    axios.get(url)
+                        .then((response) => {
+
+                            this.messages = response.data;
+
+                        });
+
+                },
                 send() {
-        
+
                     const url = '/ajax/chat';
                     const params = { message: this.message };
                     axios.post(url, params)
                         .then((response) => {
-        
+
                             // 成功したらメッセージをクリア
                             this.message = '';
-        
+
                         });
-        
+
                 }
+            },
+            mounted() {
+
+                this.getMessages();
+
             }
         });
 
