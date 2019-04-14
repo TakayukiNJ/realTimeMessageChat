9aff0a21df299d62281d636e036ff761e9b69730
NJTAKAYUKI <nj.takayuki@gmail.com>
2019-04-14 14:42:01 +0000

    Ajaxでメッセージをデータベースに保存するまでできた

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('chat', 'ChatController@index');
+
+Route::get('ajax/chat', 'Ajax\ChatController@index'); // メッセージ一覧を取得
+Route::post('ajax/chat', 'Ajax\ChatController@create'); // チャット登録
